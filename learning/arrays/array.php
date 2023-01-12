5fb6c730c64cb72ea7e8d3442462e8cb3c01e7b0
Pierre M <109650634+Gollumeo@users.noreply.github.com>
2023-01-12 11:30:13 +0100

    ex arrays

--- learning/arrays/array.php
+++ learning/arrays/array.php
@@ -33,3 +33,54 @@ $me['mother'] = $mother;
 echo '<pre>';
 print_r($me);
 echo '</pre>';
+
+echo "My mother has " . count($mother['hobbies']) . " hobbies.<br>";
+echo "I have " . count($me['hobbies']) . " hobbies.<br>";
+echo "We both have " . count($mother['hobbies']) + count($me['hobbies']) . " hobbies<br>";
+
+$me['hobbies'][] = "Taxidermy";
+
+$me['lastname'] = "Durand";
+print_r($me['lastname']);
+
+$soulmate = [
+    'age' => 27,
+    'firstname' => 'Justine',
+    'lastname' => 'Frigo',
+    'fav_movies' => ['PS: I Love You', 'Dirty Dancing', 'Percy Jackson']
+];
+
+$soulmate['hobbies'][] = "Video games";
+$soulmate['hobbies'][] = "World of Warcraft";
+$soulmate['hobbies'][] = "Taxidermy";
+
+$intersection = array_intersect($me['hobbies'], $soulmate['hobbies']);
+$merge = array_merge($me['hobbies'], $soulmate['hobbies']);
+
+echo "<pre>";
+print_r($intersection);
+print_r($merge);
+echo "</pre>";
+
+echo "<br><br>Another exercise<br>";
+$web_development = [
+    'frontend' => [],
+    'backend' => []
+];
+
+$web_development['frontend'][] = 'xhtml';
+echo "<pre>";
+print_r($web_development);
+$web_development['backend'][] = "Ruby on Rails";
+print_r($web_development);
+$web_development['frontend'][] = "CSS";
+print_r($web_development);
+$web_development['backend'][] = "Flash";
+print_r($web_development);
+$web_development['frontend'][] = "Javascript";
+print_r($web_development);
+$web_development['frontend'][0] = "html";
+print_r($web_development);
+array_splice($web_development['backend'], 1);
+print_r($web_development);
+echo "</pre>";
